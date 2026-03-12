5babdd7db053bc0c25dddae7ba9acf53efbc21ed
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-12 12:51:50 +0000

    chore: the endpoints in the api file

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    
+    Route::get('/me', [ProfileController::class, 'show']);
+    Route::put('/me', [ProfileController::class, 'update']);
+    Route::put('/me/password', [ProfileController::class, 'updatePassword']);
+    Route::delete('/me', [ProfileController::class, 'destroy']);
+});
