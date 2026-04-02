34c358415e8b098fabefbff49bd6d9d883890792
a-oirgari <abdrahmanghali1998@gmail.com>
2026-04-02 15:10:13 +0100

    refactor: the api.php for routes updated to feat OAuth

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,34 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
-
+/*
+|--------------------------------------------------------------------------
+| Public routes
+|--------------------------------------------------------------------------
+*/
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
+// OAuth routes
+Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'callback']);
 
+/*
+|--------------------------------------------------------------------------
+| Protected routes (Sanctum token required)
+|--------------------------------------------------------------------------
+*/
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    
-    Route::get('/me', [ProfileController::class, 'show']);
-    Route::put('/me', [ProfileController::class, 'update']);
+    Route::get('/me',          [ProfileController::class, 'show']);
+    Route::put('/me',          [ProfileController::class, 'update']);
     Route::put('/me/password', [ProfileController::class, 'updatePassword']);
-    Route::delete('/me', [ProfileController::class, 'destroy']);
+    Route::delete('/me',       [ProfileController::class, 'destroy']);
 });
