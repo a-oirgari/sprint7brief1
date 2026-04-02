d1d1028aad0777b751e083485975b6af83a0168d
a-oirgari <abdrahmanghali1998@gmail.com>
2026-04-02 15:13:36 +0100

    refactor: the api.php for routes updated to feat OAuth

--- routes/api.php
+++ routes/api.php
@@ -5,24 +5,16 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Public routes
-|--------------------------------------------------------------------------
-*/
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
-// OAuth routes
+
 Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirect']);
 Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'callback']);
 
-/*
-|--------------------------------------------------------------------------
-| Protected routes (Sanctum token required)
-|--------------------------------------------------------------------------
-*/
+
 
 Route::middleware('auth:sanctum')->group(function () {
 
